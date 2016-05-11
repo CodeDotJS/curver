15f603b3c6ecad2445edd5abe54f58b6057c0df7
CodeDotJS <rushforlinux@gmail.com>
2016-05-11 23:31:37 +0530

    final | func(trim)

---
+++ index.js
@@ -0,0 +1,32 @@
+'use strict';
+var got = require('got');
+var cheerio = require('cheerio');
+var Promise = require('pinkie-promise');
+
+function remuseChars(getString) {
+	const finalVerp = getString.split('is')[0].trim();
+
+	return finalVerp;
+}
+
+module.exports = function (packageName) {
+	if (typeof packageName !== 'string') {
+		return Promise.reject(new Error('package name required'));
+	}
+
+	var url = 'https://www.npmjs.com/packages/' + packageName;
+
+	return got(url).then(function (res) {
+		var $ = cheerio.load(res.body);
+
+		return {
+			name: remuseChars($('li').eq(11).text()) || null,
+		};
+	}).catch(function (err) {
+		if (err.statusCode === 404) {
+			err.message = 'Package doesn\'t exist';
+		}
+
+		throw err;
+	});
+};
